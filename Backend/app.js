b466ae9e80c1050e5437368199c16e8ec2b06523
Som Ahirwar <ahirwar.som@gmail.com>
2022-03-06 19:31:01 +0530

    Upgrading backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,14 @@
-const express=require('express'); //requiring express
+const express = require("express"); //requiring express
 //const { nextTick } = require('process');
 
-const app=express();  //invoking express with name = app
-const cookieParser=require('cookie-parser');
+const app = express(); //invoking express with name = app
+const cookieParser = require("cookie-parser");
 //middleware
 app.use(express.json());
 app.listen(3000);
 app.use(cookieParser());
 
-const userRouter=require('./Routers/userRouter');
+const userRouter = require("./Routers/userRouter");
 //const authRouter=require('./Routers/authRouter');
 //base route
-app.use('/user',userRouter);
+app.use("/user", userRouter);
